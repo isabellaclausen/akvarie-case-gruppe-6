22c5892bd9bad3b99bd53484a28b4445cc861bdd
isabellaclausen <isabellaclausen06@gmail.com>
2026-03-16 22:07:23 +0100

    krabbe udkast

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 "use strict";
+const crab = document.getElementById("krabbe");
+const crabTooltip = document.getElementById("krabbeTooltip");
+
+krabbe.addEventListener("click", () => {
+  krabbeTooltip.classList.toggle("is-visible");
+});
